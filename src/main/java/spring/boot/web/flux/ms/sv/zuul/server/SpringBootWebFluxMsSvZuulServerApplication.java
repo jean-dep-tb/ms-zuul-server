5db01ec72d4e5b66b7707084d52465df6b15488a
sistemas@gmail.com <59699335+jean-dep-tb@users.noreply.github.com>
2020-02-05 20:45:44 -0500

    balanceo
    balanceo

--- src/main/java/spring/boot/web/flux/ms/sv/zuul/server/SpringBootWebFluxMsSvZuulServerApplication.java
+++ src/main/java/spring/boot/web/flux/ms/sv/zuul/server/SpringBootWebFluxMsSvZuulServerApplication.java
@@ -2,9 +2,11 @@ package spring.boot.web.flux.ms.sv.zuul.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableCircuitBreaker
 @EnableEurekaClient
 @EnableZuulProxy
 @SpringBootApplication
